09d1e6310a0fbf0fc2f45d305626aff625792a4a
silogecho97 <silogecho@gmail.com>
2020-11-11 15:48:28 +0300

    landing finish

--- src/app/landing/components/landing-home/landing-home.component.ts
+++ src/app/landing/components/landing-home/landing-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ProducerService } from '@app/_services/identity/producer.service';
 
 @Component({
   selector: 'app-landing-home',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-home.component.scss']
 })
 export class LandingHomeComponent implements OnInit {
+  searchField: FormControl;
 
-  constructor() { }
+  constructor(private producerService: ProducerService) {}
 
   ngOnInit(): void {
+    this.searchField = new FormControl();
+
+    this.searchField.valueChanges.subscribe((term) => {
+      // this.searches.push(term);
+    });
   }
 
+  onSumbit() {
+    if (!this.searchField.value) {
+      return;
+    }
+
+    /**
+     * Search for Product
+     */
+    this.producerService.searchProducer(this.searchField.value).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
