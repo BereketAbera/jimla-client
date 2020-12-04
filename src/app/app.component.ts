54eeb5d341994a8b5893b668b9553f23c14407f2
bereket-abera <bereket5042@gmail.com>
2020-12-04 12:40:58 +0000

    feat: test pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    this.swUpdate.available.subscribe(() => {
+      console.log('----------------------new version available-------------------------------');
+      if (confirm('New Version Available. Load new Version?')) {
+        window.location.reload();
+      }
+    });
+  }
+
   title = 'jimla-client';
 }
