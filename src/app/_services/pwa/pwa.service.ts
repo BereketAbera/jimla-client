5372e9773d1f15bfb94bbae063c7c17def66e3d6
bereket-abera <bereket5042@gmail.com>
2020-12-10 09:10:36 +0000

    chore:  pwa add to home button 4

--- src/app/_services/pwa/pwa.service.ts
+++ src/app/_services/pwa/pwa.service.ts
@@ -1,5 +1,5 @@
 import { Platform } from '@angular/cdk/platform';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { PwaPromptComponent } from '@app/landing/components/pwa-prompt/pwa-prompt.component';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { timer } from 'rxjs';
@@ -10,8 +10,14 @@ import { take } from 'rxjs/operators';
 })
 export class PwaService {
   private promptEvent: any;
+  modalClose: EventEmitter<any> = new EventEmitter();
+  modalActive = false;
 
-  constructor(private modalService: NzModalService, private platform: Platform) {}
+  constructor(private modalService: NzModalService, private platform: Platform) {
+    this.modalClose.subscribe((res) => {
+      this.modalActive = false;
+    });
+  }
 
   public initPwaPrompt() {
     if (this.platform.ANDROID) {
@@ -32,14 +38,19 @@ export class PwaService {
   private openPromptComponent(mobileType: 'ios' | 'android') {
     timer(3000)
       .pipe(take(1))
-      .subscribe(() =>
-        this.modalService.create({
-          nzClosable: false,
-          nzFooter: null,
-          nzMaskClosable: false,
-          nzContent: PwaPromptComponent,
-          nzComponentParams: { mobileType, promptEvent: this.promptEvent }
-        })
-      );
+      .subscribe(() => {
+        if (!this.modalActive) {
+          this.modalService.create({
+            nzClosable: false,
+            nzFooter: null,
+            nzMaskClosable: false,
+            nzContent: PwaPromptComponent,
+            nzComponentParams: { mobileType, promptEvent: this.promptEvent },
+            nzAfterClose: this.modalClose
+          });
+        }
+
+        this.modalActive = true;
+      });
   }
 }
